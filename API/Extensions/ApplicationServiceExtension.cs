207a005f595d7e29e9ac3eecfc851a23f7df2066
AmiraliF <AmiraliF@DESKTOP-URS3PMV>
2025-07-16 12:43:45 +0330

    End of section 8

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -3,6 +3,9 @@ using API.Data;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+
+
 
 namespace API.Extensions;
 
@@ -18,6 +21,8 @@ public static class ApplicationServiceExtensions
 
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         return services;
     }
